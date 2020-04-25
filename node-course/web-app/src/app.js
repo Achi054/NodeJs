e3428c65a919b57e2f83dceef0bd2e8a495beeaa
Sujith Acharya <sujith.acharya054@yahoo.com>
2020-04-25 16:47:03 +0530

    styling and error page

--- node-course/web-app/src/app.js
+++ node-course/web-app/src/app.js
@@ -1,13 +1,16 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const wwwpath = path.join(__dirname, '../wwwroot');
-const templatepath = path.join(__dirname, '../wwwroot/templates');
+const viewspath = path.join(__dirname, '../wwwroot/templates/views');
+const partialspath = path.join(__dirname, '../wwwroot/templates/partials');
 const app = express();
 
 app.use(express.static(wwwpath));
 app.set('view engine', 'hbs');
-app.set('views', templatepath);
+app.set('views', viewspath);
+hbs.registerPartials(partialspath);
 
 app.get('', (req, res) =>
     res.render('index', {
@@ -18,22 +21,34 @@ app.get('', (req, res) =>
 
 app.get('/about', (req, res) =>
     res.render('about', {
-        title: 'About Weather',
+        title: 'About Weather!',
         name: 'Sujith Acharya'
     })
 );
 
 app.get('/help', (req, res) =>
     res.render('help', {
-        title: 'Ask me',
+        title: 'Ask me!',
         name: 'Sujith Acharya'
     })
 );
 
 app.get('/weather', (req, res) => {
-    res.send({
+    res.render('weather', {
         forecast: 'forecast',
         location: 'location'
+    });
+});
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        text: 'Help article not found'
+    });
+});
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        text: 'Page not found'
     });
 });
 
