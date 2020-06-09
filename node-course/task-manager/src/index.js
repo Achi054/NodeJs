bc1057f8f8bbdbb2bd160082b3cdbb030d44d46a
Sujith Acharya <sujith.acharya054@yahoo.com>
2020-06-09 20:13:33 +0530

    update to index.js

--- node-course/task-manager/src/index.js
+++ node-course/task-manager/src/index.js
@@ -19,3 +19,60 @@ app.post('/users', (req, res) => {
     .catch((error) => { res.status(400).send(error); })
 });
 
+app.get('/users', (req, res) => {
+    User.find()
+    .then((users) => {
+        if(!users)
+            return res.status(404).send('User not found');
+        
+        res.send(users);
+    })
+    .catch((error) => res.status(500).send(error));
+});
+
+app.get('/users/:id', (req, res) => {
+    var id = req.params.id;
+
+    User.findById(id)
+    .then((user) => {
+        if(!user)
+            return res.status(404).send('User not found');
+        
+        res.send(user);
+    })
+    .catch((error) => res.status(500).send(error));
+});
+
+// Task API(s)
+app.post('/tasks', (req, res) => {
+    var task = new Task(req.body);
+
+    task.save()
+    .then(() => res.send(task))
+    .catch((error) => res.status(400).send(error));
+});
+
+app.get('/tasks', (req, res) => {
+    Task.find()
+    .then((tasks) => {
+        if(!tasks)
+            return res.status(404).send('No tasks available.');
+        
+        res.send(tasks);
+    })
+    .catch((error) => res.status(500).send(error));
+});
+
+app.get('/tasks/:id', (req, res) => {
+    var id = req.params.id;
+
+    Task.findById(id)
+    .then((task) => {
+        if(!task)
+            return res.status(404).send('Task not found');
+        
+        res.send(task);
+    })
+    .catch((error) => res.status(500).send(error));
+});
+
