b273b16daac479c3917c6ae9d04ff0623cff80cd
Sujith Acharya <sujithacharya@eurofins.com>
2020-06-17 12:49:38 +0530

    jwt token

--- node-course/task-manager/src/models/user.js
+++ node-course/task-manager/src/models/user.js
@@ -1,65 +1,81 @@
 var mongoose = require('mongoose');
 var validate = require('validator');
 var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
 var userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        require: true,
-        trim: true,
-        minlength: [7, 'Password length should be more than 7 characters.'],
-        validate(value) {
-            if (validate.contains(value.toLowerCase(), 'password'))
-                throw new Error('Password not strong enough.');
-        },
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        lowercase: true,
-        trim: true,
-        validate(value) {
-            if (!validate.isEmail(value)) throw new Error('Invalid email.');
-        },
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) throw new Error('Age should be a positive number.');
-        },
-    },
+	name: {
+		type: String,
+		trim: true,
+		required: true,
+	},
+	password: {
+		type: String,
+		require: true,
+		trim: true,
+		minlength: [7, 'Password length should be more than 7 characters.'],
+		validate(value) {
+			if (validate.contains(value.toLowerCase(), 'password'))
+				throw new Error('Password not strong enough.');
+		},
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: true,
+		lowercase: true,
+		trim: true,
+		validate(value) {
+			if (!validate.isEmail(value)) throw new Error('Invalid email.');
+		},
+	},
+	age: {
+		type: Number,
+		default: 0,
+		validate(value) {
+			if (value < 0) throw new Error('Age should be a positive number.');
+		},
+	},
+	tokens: [
+		{
+			token: {
+				type: String,
+				require: true,
+			},
+		},
+	],
 });
 
+userSchema.methods.generateAuthToken = async function () {
+	var user = this;
+	var token = jwt.sign({ _id: user._id.toString() }, 'thisismynodeapp');
+	user.tokens = user.tokens.concat({ token });
+	await user.save();
+
+	return token;
+};
+
 userSchema.statics.findByCredential = async (email, password) => {
-    var user = await User.findOne({
-        email
-    });
+	var user = await User.findOne({
+		email,
+	});
 
-    if (!user)
-        throw new Error('Unable to login');
+	if (!user) throw new Error('Unable to login');
 
-    var isMatch = bcrypt.compare(user.password, password);
+	var isMatch = bcrypt.compare(user.password, password);
 
-    if (!isMatch)
-        throw new Error('Unable to login');
+	if (!isMatch) throw new Error('Unable to login');
 
-    return user;
+	return user;
 };
 
 userSchema.pre('save', async function (next) {
-    var user = this;
+	var user = this;
 
-    if (user.isModified('password'))
-        user.password = await bcrypt.hash(user.password, 8);
+	if (user.isModified('password'))
+		user.password = await bcrypt.hash(user.password, 8);
 
-    next();
+	next();
 });
 
 var User = new mongoose.model('users', userSchema);
